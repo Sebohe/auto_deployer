be2e3b3f99f6d1a6bec32956a839b38f2e2e554e
Sebastian Bolanos <sebasheston@gmail.com>
2018-04-04 13:11:19 -0700

    added proper address checksum

--- addresses.py
+++ addresses.py
@@ -1,9 +1,9 @@
 # Functions that generates addresses
 from ethereum.utils import (
         mk_contract_address,
-        sha3,
-        normalize_address,
-        encode_hex
+        encode_hex,
+        decode_hex,
+        checksum_encode
     )
 
 def determineContractAddr(deployerAddr, tx_count, qty=1):
@@ -12,6 +12,6 @@ def determineContractAddr(deployerAddr, tx_count, qty=1):
     for i in range(tx_count, tx_count + qty):
         _addr = str(encode_hex(mk_contract_address(deployerAddr, i)))
         _addr = _addr.replace("\'", "")
-        _addr = "0x" + _addr[1:]
+        _addr = checksum_encode(decode_hex(_addr))
 
     return _addr
